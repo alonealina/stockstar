ac924ed93149ef72507cbc663672a5d09bc0a588
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-01-28 21:38:49 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ini_set('mbstring.internal_encoding' , 'UTF-8');
+        Schema::defaultStringLength(191);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
